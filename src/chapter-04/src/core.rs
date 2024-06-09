220840e03ed4859b4808e8b083e4d7b17cfbcfd8
Luciano Mammino <lucianomammino@gmail.com>
2024-06-09 14:05:06 +0100

    chore(chapter4): first iteration of solution

--- src/chapter-04/src/core.rs
+++ src/chapter-04/src/core.rs
@@ -1,7 +1,6 @@
+use aws_sdk_dynamodb::{types::AttributeValue, Client};
 use cuid2::CuidConstructor;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::sync::RwLock;
 
 #[derive(Deserialize)]
 pub struct ShortenUrlRequest {
@@ -13,30 +12,63 @@ pub struct ShortenUrlResponse {
     shortened_url: String,
 }
 
-#[derive(Default, Debug)]
+#[derive(Debug)]
 pub struct UrlShortener {
-    urls: RwLock<HashMap<String, String>>,
+    dynamodb_urls_table: String,
+    dynamodb_client: Client,
 }
 
 impl UrlShortener {
-    pub fn new() -> Self {
-        Self::default()
+    pub fn new(dynamodb_urls_table: String, dynamodb_client: Client) -> Self {
+        Self {
+            dynamodb_urls_table,
+            dynamodb_client,
+        }
     }
 
-    pub fn shorten_url(&self, req: ShortenUrlRequest) -> Result<ShortenUrlResponse, ()> {
+    pub async fn shorten_url(&self, req: ShortenUrlRequest) -> Result<ShortenUrlResponse, String> {
         let short_url = self.generate_short_url();
 
-        let mut map = self.urls.write().unwrap();
-        map.insert(short_url.clone(), req.url_to_shorten);
+        let result = self
+            .dynamodb_client
+            .put_item()
+            .table_name(&self.dynamodb_urls_table)
+            .item("LinkId", AttributeValue::S(short_url.clone()))
+            .item(
+                "OriginalLink",
+                AttributeValue::S(req.url_to_shorten.clone()),
+            )
+            .item("Clicks", AttributeValue::N("0".to_string()))
+            .condition_expression("attribute_not_exists(LinkId)")
+            .send()
+            .await;
 
-        Ok(ShortenUrlResponse {
-            shortened_url: short_url,
-        })
+        match result {
+            Ok(_) => Ok(ShortenUrlResponse {
+                shortened_url: short_url,
+            }),
+            Err(e) => return Err(format!("Failed to shorten URL: {:?}", e)),
+        }
     }
 
-    pub fn retrieve_url(&self, short_url: String) -> Option<String> {
-        let map = self.urls.read().unwrap();
-        map.get(&short_url).cloned()
+    pub async fn retrieve_url(&self, short_url: String) -> Result<Option<String>, String> {
+        let result = self
+            .dynamodb_client
+            .get_item()
+            .table_name(&self.dynamodb_urls_table)
+            .key("LinkId", AttributeValue::S(short_url.clone()))
+            .send()
+            .await;
+
+        result
+            .map_err(|e| format!("Error getting item: {:?}", e))
+            .map(|record| {
+                record.item.and_then(|attributes| {
+                    attributes
+                        .get("OriginalLink")
+                        .map(|v| v.as_s().unwrap().clone())
+                })
+            })
     }
 
     fn generate_short_url(&self) -> String {
--- src/chapter-04/src/main.rs
+++ src/chapter-04/src/main.rs
@@ -4,6 +4,7 @@ use lambda_http::http::StatusCode;
 use lambda_http::{
     run, service_fn, tracing, Error, IntoResponse, Request, RequestExt, RequestPayloadExt, Response,
 };
+use std::env;
 
 mod core;
 mod utils;
@@ -21,12 +22,14 @@ async fn function_handler(
             match shorten_url_request_body {
                 None => generate_api_response(400, "".to_string()),
                 Some(shorten_url_request) => {
-                    let shortened_url_response = url_shortener.shorten_url(shorten_url_request);
+                    let shortened_url_response =
+                        url_shortener.shorten_url(shorten_url_request).await;
 
                     let response = match shortened_url_response {
                         Ok(response) => {
                             generate_api_response(200, serde_json::to_string(&response).unwrap())?
                         }
+                        // TODO: handle error better (this should be a 500)
                         Err(_) => generate_api_response(400, "Bad request".to_string())?,
                     };
 
@@ -40,11 +43,18 @@ async fn function_handler(
                 .and_then(|params| params.first("shortUrl"))
                 .unwrap_or("");
 
-            let full_url = url_shortener.retrieve_url(short_url.to_string());
+            let full_url = url_shortener.retrieve_url(short_url.to_string()).await;
 
             match full_url {
-                None => Ok(generate_api_response(404, "".to_string())?),
-                Some(url) => {
+                Err(e) => {
+                    tracing::error!("Failed to retrieve URL: {:?}", e);
+                    Ok(generate_api_response(
+                        500,
+                        "Internal Server Error".to_string(),
+                    )?)
+                }
+                Ok(None) => Ok(generate_api_response(404, "".to_string())?),
+                Ok(Some(url)) => {
                     let response = Response::builder()
                         .status(StatusCode::from_u16(302).unwrap())
                         .header("Location", url)
@@ -63,7 +73,10 @@ async fn function_handler(
 async fn main() -> Result<(), Error> {
     tracing::init_default_subscriber();
 
-    let shortener = UrlShortener::new();
+    let table_name = env::var("TABLE_NAME").expect("TABLE_NAME is not set");
+    let config = aws_config::load_from_env().await;
+    let dynamodb_client = aws_sdk_dynamodb::Client::new(&config);
+    let shortener = UrlShortener::new(table_name, dynamodb_client);
 
     run(service_fn(|event| function_handler(&shortener, event))).await
 }
