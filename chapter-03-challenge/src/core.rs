790de27e89253bdcb81c945b58ad19731c2e4caf
James Eastham <dev@jameseastham.co.uk>
2025-02-03 17:56:17 +0000

    Chapter 03 code updates and add challenge (#13)
    * #52 Update to match new cargo lambda init format
    
    * #57 add default samconfig options
    
    * #55 add chapter03 challenge code
    
    * #57 include samconfig in gitignore

---
+++ chapter-03-challenge/src/core.rs
@@ -0,0 +1,51 @@
+use cuid2::CuidConstructor;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::RwLock;
+
+#[derive(Deserialize)]
+pub struct ShortenUrlRequest {
+    url_to_shorten: String,
+}
+
+#[derive(Serialize)]
+pub struct ShortenUrlResponse {
+    shortened_url: String,
+}
+
+#[derive(Default, Debug)]
+pub struct UrlShortener {
+    urls: RwLock<HashMap<String, String>>,
+}
+
+impl UrlShortener {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn shorten_url(&self, req: ShortenUrlRequest) -> ShortenUrlResponse {
+        let short_url = self.generate_short_url();
+
+        let mut map = self.urls.write().unwrap();
+        map.insert(short_url.clone(), req.url_to_shorten);
+
+        ShortenUrlResponse {
+            shortened_url: short_url,
+        }
+    }
+
+    pub fn get_links(&self) -> Vec<String> {
+        let map = self.urls.read().unwrap();
+        map.keys().cloned().collect()
+    }
+
+    pub fn retrieve_url(&self, short_url: &str) -> Option<String> {
+        let map = self.urls.read().unwrap();
+        map.get(short_url).cloned()
+    }
+
+    fn generate_short_url(&self) -> String {
+        let idgen = CuidConstructor::new().with_length(10);
+        idgen.create_id()
+    }
+}
